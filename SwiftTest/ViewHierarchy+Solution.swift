ece9859dc13cec87d8b76e875f7665edb995c4b6
Vlad Z <vladislavz1991@gmail.com>
2020-01-23 10:44:57 -0500

    cleanup

--- SwiftTest/ViewHierarchy+Solution.swift
+++ SwiftTest/ViewHierarchy+Solution.swift
@@ -5,33 +5,24 @@ extension Node {
     //if using Swift, implement this function
     static func findPathToNode(rootNode: Node, drawnAtPixel pixel: CGPoint) -> [String]{
         let result = findFirstNode(in: rootNode, drawnAtPixel: pixel)
-
-        let ids = result
-            .filter({ !$0.frame.intersects(rootNode.frame) })
-            .map { $0.nodeId }
         
-        print("test is here \(rootNode.frame.contains(pixel))")
-        print("ids is here - \(ids)")
-        
-        return ids.count > 0 ? ids : rootNode.frame.contains(pixel) ? [rootNode.nodeId] : []
+        return result.map { $0.nodeId }
     }
-
-    static private func findFirstNode(in node: Node, drawnAtPixel pixel: CGPoint)-> [Node] {
+    
+    static private func findFirstNode(in node: Node,
+                                      drawnAtPixel pixel: CGPoint)-> [Node] {
         var queue = Queue<Node>()
         queue.enqueue(node)
         
         var result: [Node] = []
         while !queue.isEmpty {
-            let dequeuedNode = queue.dequeue()
-            if let dequeuedNode = dequeuedNode {
-                if dequeuedNode.frame.contains(pixel) {
-                    result.append(dequeuedNode)
-                }
-                
-                let childrenIntersected = getTopChildrenIntersection(for: dequeuedNode.children, in: pixel)
-                
-                queue.enqueue(childrenIntersected)
-            }
+            guard let dequeuedNode = queue.dequeue() else { break }
+            if dequeuedNode.frame.contains(pixel) { result.append(dequeuedNode) }
+            
+            let childrenIntersected = getTopChildrenIntersection(for: dequeuedNode.children,
+                                                                 in: pixel)
+            
+            queue.enqueue(childrenIntersected)
         }
         
         return result
@@ -40,24 +31,35 @@ extension Node {
     static private func getTopChildrenIntersection(for children: [Node],
                                                    in pixel: CGPoint)-> [Node] {
         children.reduce([] as [Node]) { result, element in
-                                var tempNode: Node?
-            let check = result.contains(where: { resultElement in
-                if element.frame.intersects(resultElement.frame) &&
-                    element.frame.contains(pixel) &&
-                    resultElement.frame.contains(pixel) {
-                    tempNode = element
-                    
-                    return true
-                }
-                
-                return false
-            })
-            
-            if check {
-                return tempNode != nil ? [tempNode!] : result
+            let (isChildrenIntersect, topIntersectionNode) = checkIfChildrenIntersect(nodes: result,
+                                                                                      currentElement: element,
+                                                                                      pixel: pixel)
+            if isChildrenIntersect {
+                /// If children intersect - return top most, otherwise current result
+                return topIntersectionNode != nil ? [topIntersectionNode!] : result
             }
             
-            return result + (tempNode != nil ? [] : [element])
+            return result + [element]
         }
+    }
+    
+    static private func checkIfChildrenIntersect(nodes: [Node],
+                                                 currentElement: Node,
+                                                 pixel: CGPoint)-> (Bool, Node?) {
+        var tempNode: Node?
+        let checkCondition = nodes.contains(where: { resultElement in
+            if currentElement.frame.intersects(resultElement.frame),
+                currentElement.frame.contains(pixel),
+                resultElement.frame.contains(pixel) {
+                tempNode = currentElement
+                
+                return true
+            }
+            
+            return false
+        })
+        
+        return (checkCondition,
+                tempNode)
     }
 }
